df7fd94c09496510725c41c542b16ee8a9677222
atdilakshan <dilakshan0501@gmail.com>
2023-03-23 15:12:10 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrandComponent } from './components/header/brand/brand.component';
+import { AvatarComponent } from './components/header/avatar/avatar.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BrandComponent,
+    AvatarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
